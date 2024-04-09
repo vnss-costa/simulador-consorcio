47d5da7cf8a38f3ce06cf306c42c2ad7ee1ad29d
vnss-costa <vanessa.costa@bpoconsig.com.br>
2024-04-08 21:21:15 -0300

    adiciona o header as infos para calculo

--- src/pages/Simulate.jsx
+++ src/pages/Simulate.jsx
@@ -5,6 +5,8 @@ import '../css/Styles.css';
 import Button from "../components/Button";
 import SelectDropdown from "../components/SelectDropdown";
 import Info from "../components/Info";
+import Input from "../components/Input";
+import Header from "../components/Header";
 
 const Simulate = () => {
 
@@ -12,8 +14,13 @@ const Simulate = () => {
     const value = params.get('value');
 
     const [selectedValue, setSelectedValue] = useState('');
+    const [parcelas, setParcelas] = useState('');
 
     const options = Array.from({length: 24}, (_, i) => i + 1);
+
+    const handleParcelasChange = (event) => {
+      setParcelas(event.target.value);
+      };
     
     const handleChange = (event) => {
         setSelectedValue(event.target.value);
@@ -23,18 +30,29 @@ const Simulate = () => {
     console.log('Botão clicado');  
     };
 
+    console.log(options)
+
   return (
     <div className='body'>
+      <Header></Header>
       <div className='title'>
         <h1>Simulação</h1>
       </div>
       <div className='container'>
         <Info value={value}/>
-        {/* <SelectDropdown options={automoveis} value={selectedOption} onChange={handleSelectChange} /> */}
-        {/* <SelectDropdown options={automoveis} value={selectedOption} onChange={handleSelectChange} /> */}
+        <SelectDropdown 
+          options={options.map(option => ({ value: option, label: option }))} 
+          value={selectedValue} 
+          onChange={handleChange} 
+          defaultLabel={'Selecione o mês a ser contemplado'}
+        />
+        <Input 
+            value={parcelas}
+            onChange={handleParcelasChange}
+            placeholder="Digite o número de parcelas (1-54)"
+        />
         <div className='buttons-cage'>
           <Button children={'Simular'} onClick={handleButtonClick}/>
-          <Button children={'Contato'} onClick={handleButtonClick}/>
         </div>
       </div>
     </div>
