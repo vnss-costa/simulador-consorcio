5f60294b392d436efb538d0e8e0f394118b0e590
vnss-costa <vanessa.costa@bpoconsig.com.br>
2024-04-08 21:24:40 -0300

    adiciona o header

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,57 +4,57 @@ import '../css/Styles.css';
 
 import Button from "../components/Button";
 import Input from "../components/Input";
+import Header from "../components/Header";
+import PopUp from "../components/Popup";
 
 const Contact = () => {
+    const [formData, setFormData] = useState({ name: '', email: '', celular: '' });
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [celular, setCelular] = useState('');
-
-    const handleNameChange = (event) => {
-    setName(event.target.value);
-    };
-
-    const handleEmailChange = (event) => {
-    setName(event.target.value);
-    };
-
-    const handleCelularChange = (event) => {
-    setCelular(event.target.value);
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(prevData => ({ ...prevData, [name]: value }));
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("Formulário enviado com sucesso!");
+        setIsPopupOpen(true);
+    };
+
+    const handleClosePopUp = () => {
+        setIsPopupOpen(false);
     };
 
     return (
-    <div className='body'>
-        <div className='title'>
-        <h1>Deixe seu contato!</h1>
-        </div>
-        <form className='container' onSubmit={handleSubmit}>
-            <div>
+        <div className='body'>
+            <Header/>
+            <div className='title'>
+                <h1>Deixe seu contato!</h1>
+            </div>
+            <form className='container' onSubmit={handleSubmit}>
                 <Input 
-                    value={name}
-                    onChange={handleNameChange}
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    name='name'
                     placeholder="Nome"
                 />
                 <Input 
-                    value={email}
-                    onChange={handleEmailChange}
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    name='email'
                     placeholder="Email"
                 />
                 <Input 
-                    value={celular}
-                    onChange={handleCelularChange}
+                    value={formData.celular}
+                    onChange={handleInputChange}
+                    name='celular'
                     placeholder="Telefone"
                 />
                 <Button children={'Enviar'} onClick={handleSubmit}/>
-            </div>
-        </form>
-    </div>
-  );
+                {isPopupOpen && <PopUp message={'Dados enviados com sucesso'} onClose={handleClosePopUp}/>}
+            </form>
+        </div>
+    );
 };
 
 export default Contact;
